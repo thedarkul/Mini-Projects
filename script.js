69187accf019590dfc233d218416fb43f5ec7dc0
Alif Hossain <alifhossain5577@gmail.com>
2023-09-29 11:33:56 +0600

    added 3 projects

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Project Added
+const numberOfProject = document.querySelectorAll("#projectList li").length;
+// console.log("Number of <li> elements:", numberOfProject);
+
+const projectAdded = document.querySelector("#projectAdded");
+
+projectAdded.textContent = numberOfProject;
+
+// END
